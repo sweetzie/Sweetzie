f8a97eb2be1eda66b98b34e84ec5f7db9324a296
Kyle Doherty <kyle@sweetzie.com>
2010-08-17 19:06:08 -0400

    Added relationship model and raked database

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100817194304) do
+ActiveRecord::Schema.define(:version => 20100817230508) do
+
+  create_table "relationships", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "relation_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "login",                     :limit => 40
