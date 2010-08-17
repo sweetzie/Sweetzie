bd1e9119762ddd287ce5e31d332555602cedaad3
Kyle Doherty <kyle@sweetzie.com>
2010-08-17 19:04:10 -0400

    Added show and index methods to users. Added relationships controller for beginning of following relationships

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -27,14 +27,14 @@ class SessionsController < ApplicationController
 
   def destroy
     logout_killing_session!
-    flash[:notice] = "You have been logged out."
+    flash[:notice] = "You have been logged out. Thanks for using Sweetzie!"
     redirect_back_or_default('/')
   end
 
 protected
   # Track failed login attempts
   def note_failed_signin
-    flash[:error] = "Couldn't log you in as '#{params[:login]}'"
+    flash[:error] = "Couldn't log you in as #{params[:login]}"
     logger.warn "Failed login for '#{params[:login]}' from #{request.remote_ip} at #{Time.now.utc}"
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
   
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
   # render new.rhtml
   def new
     @user = User.new
