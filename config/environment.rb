75d7d2e03b1eb7fa6d72880234965e81e88fc297
Kyle Doherty <kyle@sweetzie.com>
2010-08-17 17:01:29 -0400

    Emails able to be sent

--- config/environment.rb
+++ config/environment.rb
@@ -41,3 +41,6 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+
+require 'tlsmail'
+Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,12 +1,12 @@
 # config/initializers/mail.rb
-
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   
   :address => "smtp.gmail.com",
-  :port => 993,
+  :port => 587,
   :domain => "#{SITE_URL}",
-  :authentication => :login,
+  :authentication => :plain,
+  :enable_starttls => true,
   :user_name => "kyle.l.doherty@gmail.com",
   :password =>  "valenciaCA2012"
   
