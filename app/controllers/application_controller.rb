5cb92536af3c1fc1de978478f5adf5d372d33728
Kyle Doherty <kyle@sweetzie.com>
2010-08-17 15:55:34 -0400

    Changed location of AuthenticatedSystem include from individual controllers to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  
+  # Be sure to include AuthenticationSystem in Application Controller instead
+  include AuthenticatedSystem
+  
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
