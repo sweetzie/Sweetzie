4c8ccea0b4d24752127ecda7266b3f35f57d5037
Kyle Doherty <kyle@sweetzie.com>
2010-08-31 17:32:02 -0400

    Final commit before move to california

--- app/helpers/render_helper.rb
+++
@@ -1,7 +0,0 @@
-module RenderHelper
-  def render_timeline(events)
-    events.map do |event|
-      render(:partial => "timeline_events/#{event.event_type}", :object => event)
-    end.join
-  end
-end
