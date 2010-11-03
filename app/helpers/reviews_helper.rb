78e335bde37cf2b89d0181f4d4d5a4e8e6c45dd3
Kyle Doherty <kyle.l.doherty@gmail.com>
2010-11-03 00:28:50 -0700

    Updated review system and activity stream to reflect review changes

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,6 +1,13 @@
 module ReviewsHelper
+  
   def find_review(id)
     review = Review.find_by_id(id)
-    return review.item.name
+    return review
   end
+  
+  def find_review_item(id)
+    review = Review.find_by_id(id)
+    return review.item
+  end
+  
 end
